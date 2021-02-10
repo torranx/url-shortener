0c2c6ccfb073847a1c0dad315251fdd7119f1ae7
torranicles <torranicles@gmail.com>
2021-02-11 02:39:43 +0800

    Port

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port process.env.PORT')
 });
 
 connection.db.once('open', () => {
