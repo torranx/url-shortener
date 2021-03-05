e09dc21d2d819d4bb3d19b758e2f3bfe4dea9c16
torranicles <torranicles@gmail.com>
2021-03-06 00:07:30 +0800

    Remove bodyparser

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
@@ -9,11 +8,11 @@ const connection = require('./connection');
 app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
     extended: false
 }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port '+ process.env.PORT || 3000)
